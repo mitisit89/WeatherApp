67b455b92069cd1e2d3380f695f74ee0331adb56
MitiSit89 <mitisit89@gmail.com>
2021-08-28 14:23:23 +0300

    work on api and clean up template

--- src/api/index.js
+++ src/api/index.js
@@ -2,33 +2,36 @@ import Vue from "vue";
 import Vuex from "vuex";
 const API_key = "a70e5ca8827286c01fa0d2fd3272043d";
 
-
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   actions: {
     async getLocation(context, location_name) {
-      console.log(location_name.coordinates);
-      const headers = {"Content-Type": "application/json;charset=UTF-8"};
-      const response = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${location_name.coordinates}&limit=5&appid=${API_key}`);
-      const geo_codes = await response.json()
-      console.log(geo_codes);
-      context.commit('getForecast',geo_codes)
+      const geo_cords = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${location_name.coordinates}&limit=5&appid=${API_key}`
+      );
+      const location = await geo_cords.json();
+
+      const lat = location.lat;
+      const lon = location.lon;
+      const forecast = await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&units=metric&appid=${API_key}`
+      );
+
+      const weather = await forecast.json();
+      context.commit("update", weather);
     },
-    async getForecast(context) {
+    /* async getForecast(context) {
       console.log(context);
-      //const geo_codes=await context.dispatch("getLocation")
+      await context.dispatch("getLocation")
       let lat = geo_codes.lat;
       let lon = geo_codes.lon;
-      const headers = {"Content-Type": "application/json;charset=UTF-8"};
-
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&units=metric&appid=${API_key}`);
       const weather = await response.json();
       console.log(weather);
       context.commit("update", weather);
-    },
+     },*/
   },
   mutations: {
     update(state, weather) {
--- src/components/DailyForecastCard.vue
+++ src/components/DailyForecastCard.vue
@@ -8,38 +8,6 @@
         </div>
       </div>
     </div>
-    <div class="card">
-      <div class="card-content is-mobile">
-        <div class="title">
-          <i class="fas fa-bolt"></i>
-          <i class="fas fa-plus">9</i>
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-content is-mobile">
-        <div class="title">
-          <i class="fas fa-bolt"></i>
-          <i class="fas fa-plus">9</i>
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-content is-mobile">
-        <div class="title">
-          <i class="fas fa-bolt"></i>
-          <i class="fas fa-plus">9</i>
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-content is-mobile">
-        <div class="title">
-          <i class="fas fa-bolt"></i>
-          <i class="fas fa-plus">9</i>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
